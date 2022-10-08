034fc530e5a859398e7dbbe8e9eacfc001ed9ff0
Vajira <vajirawke@gmail.com>
2022-10-08 19:14:19 +0530

    data type

--- host.js
+++ host.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 let schema = new mongoose.Schema({    
     original_url :{type:String},
-    short_url:{type:String}
+    short_url:{type:Number}
 })
 
 let host = mongoose.model('url',schema)
